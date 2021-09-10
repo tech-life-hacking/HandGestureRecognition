465a2a89531a9bd4abd7cafea449a2c0e8cd0128
tech-life-hacking <techlife.hacking@gmail.com>
2021-09-11 04:18:00 +0900

    add imshow

--- example.py
+++ example.py
@@ -53,8 +53,25 @@ if __name__ == "__main__":
         # record handlandmarks
         # myhands.record(results, writer)
 
+        # For RaspberryPi
         # print a kind of hands
         print(kind_of_hands)
+
+        # For PC
+        # Draw the hand annotations on the image.
+        # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # if results.multi_hand_landmarks:
+        #     for hand_landmarks in results.multi_hand_landmarks:
+        #         mp_drawing.draw_landmarks(
+        #             image, hand_landmarks,
+        #             mp_hands.HAND_CONNECTIONS,
+        #             mp_drawing_styles.get_default_hand_landmarks_style(),
+        #             mp_drawing_styles.get_default_hand_connections_style())
+        #         cv2.putText(image, kind_of_hands,
+        #             (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 2)
+        # cv2.imshow('MediaPipe Hands', image)
+        # if cv2.waitKey(5) & 0xFF == 27:
+        #     break
 
     cap.release()
     # For recording
